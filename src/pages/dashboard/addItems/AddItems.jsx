2b5da589d5567a33db2843b07f0063a2cb92de73
sohag55rana <sohag55rana@gmail.com>
2025-05-06 19:24:14 +0600

    added new components

--- src/pages/dashboard/addItems/AddItems.jsx
+++ src/pages/dashboard/addItems/AddItems.jsx
@@ -1,10 +1,29 @@
 import SectionTitle from "../../sheard/sectionTitle/SectionTitle";
-
+import { useForm } from "react-hook-form";
 
 const AddItems = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <div>
             <SectionTitle subHeading="what's new" heading="add an item"></SectionTitle>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("name")} />
+
+                <select  {...register("category")} defaultValue="category" className="select">
+
+                    <option disabled value='category'>Select a Category</option>
+                    <option value="BMW">BMW</option>
+                    <option value="honda">Honda</option>
+                    <option value="kawasaki">Kawasaki</option>
+                    <option value="royal">Royal Enfield</option>
+                    <option value="yamaha">Yamaha</option>
+                </select>
+
+                <input type="submit" />
+            </form>
         </div>
     );
 };
